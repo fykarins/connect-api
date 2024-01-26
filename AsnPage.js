3b578abf26a8caec2e81ead995c9b3ad5e6a5e50
Fay Ivana Karin <145007482+fykarins@users.noreply.github.com>
2024-01-26 13:21:12 +0700

    rapikan

--- AsnPage.js
+++ AsnPage.js
@@ -283,7 +283,7 @@ export const AsnPage = () => {
               </Form.Group>             
               {user.purch_org !== null && (
                 <Form.Group as={Row} className="mt-3">
-                  <Form.Label column sm={6}>
+                  <Form.Label column sm={3}>
                     <b>Purchasing Organization</b>
                   </Form.Label>
                     <Col sm={6}>
@@ -298,7 +298,7 @@ export const AsnPage = () => {
               )}
               {user.purch_org === null && (
                 <Form.Group as={Row} className="mt-3">
-                  <Form.Label column sm={6}>
+                  <Form.Label column sm={3}>
                     <b>Purchasing Organization</b>
                   </Form.Label>
                     <Col sm={6}>
